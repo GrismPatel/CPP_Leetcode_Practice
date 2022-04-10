b2b56a7a1d6be96d5c5ecaa607295550c611668a
Grism Patel <grismpatel@hotmail.com>
2022-04-10 18:04:55 -0400

    leet code question Build_Array_from_Permutation

---
+++ Build_Array_from_Permutation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> buildArray(vector<int> &nums) {
+    const int array_size = nums.size();
+    vector<int> ans(array_size);
+    for (int index =0; index <array_size; index++) {
+        ans[index] = nums[nums[index]];
+    }
+    cout << "build array: " << sizeof(nums) << endl;
+    return ans;    
+}
+
+int main() {
+    vector<int> vector1 = {0,2,1,5,3,4};
+
+    vector<int> ans = buildArray(vector1);
+
+    // ranged loop
+    for (const int& i : ans) {
+        cout << i << "  ";
+    }
+
+    return 0;
+}
